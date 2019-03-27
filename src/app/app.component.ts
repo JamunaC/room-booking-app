036daf38eab36dd13abc29b8fdbcb0b8305e3844
Jamuna <jamuna@k-arogia.com>
2019-03-28 00:28:53 +0530

    app functionality added

---
+++ src/app/app.component.ts
@@ -0,0 +1,94 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'room-booking-app';
+  numberOfRooms: number = 1;
+  numberOfAdults: number = 1;
+  numberOfChildren: number = 0;
+
+  decreaseRooms() {
+    if (this.numberOfRooms > 1) {
+      this.numberOfRooms = this.numberOfRooms - 1;
+      let sumOfPeople = this.numberOfAdults + this.numberOfChildren;
+      if(sumOfPeople >= (4*this.numberOfRooms)){
+        sumOfPeople = 4*this.numberOfRooms;
+        if(this.numberOfChildren > 0) {
+          this.numberOfChildren = sumOfPeople - this.numberOfChildren; 
+        }
+      }
+     
+    }
+  }
+
+  increaseRooms() {
+    if (this.numberOfRooms < 5) {
+      this.numberOfRooms = this.numberOfRooms + 1;
+      if(this.numberOfAdults < this.numberOfRooms)
+      this.numberOfAdults = this.numberOfAdults + 1;
+    }
+  }
+
+  decreaseAdults() {
+    
+    if (this.numberOfAdults > 1) {
+      this.numberOfAdults = this.numberOfAdults - 1;
+      let sumOfPeople = this.numberOfAdults + this.numberOfChildren;
+      let fourMultiplies = 4 * (this.numberOfRooms - 1);
+      if(this.numberOfRooms > this.numberOfAdults) {
+        this.numberOfRooms = this.numberOfRooms - 1;
+        if(this.numberOfChildren > (3*this.numberOfAdults)) {
+          this.numberOfChildren = 3*this.numberOfAdults;
+        }
+      }
+      else if(sumOfPeople < fourMultiplies + 1) {
+        this.numberOfRooms = this.numberOfRooms - 1;
+      } 
+    }
+
+
+  }
+
+  increaseAdults() {
+    let sumOfPeople = this.numberOfAdults + this.numberOfChildren;
+    if (sumOfPeople < 20) {
+      this.numberOfAdults = this.numberOfAdults + 1;
+
+      let fourMultiplies = 4 * this.numberOfRooms;
+      if (sumOfPeople == (fourMultiplies)) {
+        this.numberOfRooms = this.numberOfRooms + 1;
+      }
+    }
+
+  }
+
+  decreaseChildren() {
+    if (this.numberOfChildren > 0) {
+      this.numberOfChildren = this.numberOfChildren - 1;
+      let sumOfPeople = this.numberOfAdults + this.numberOfChildren;
+      let fourMultiplies = 4 * (this.numberOfRooms - 1);
+      if(sumOfPeople < fourMultiplies + 1) {
+        this.numberOfRooms = this.numberOfRooms - 1;
+      } 
+    }
+  }
+
+  increaseChildren() {
+    let sumOfPeople = this.numberOfAdults + this.numberOfChildren;
+    if (sumOfPeople < 20) {
+      this.numberOfChildren = this.numberOfChildren + 1;
+      let fourMultiplies = 4 * this.numberOfRooms;
+      let threeMultiplies = 3 * this.numberOfAdults;
+      if(this.numberOfChildren == (threeMultiplies + 1)) {
+        this.numberOfAdults = this.numberOfAdults + 1;
+      }
+      if (sumOfPeople == (fourMultiplies)) {
+        this.numberOfRooms = this.numberOfRooms + 1;
+      }
+    }
+  }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
